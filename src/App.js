e7bee46d351f288a3b6a03583fc37fea5e6929bf
Tom Stine <13zebras@users.noreply.github.com>
2024-01-30 23:34:45 -0600

    Update App.js - remove twitter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import twitterLogo from "./assets/twitter-logo.svg";
+// import twitterLogo from "./assets/twitter-logo.svg";
 import "./App.css";
 import idl from "./idl.json";
 import { Connection, PublicKey, clusterApiUrl } from "@solana/web3.js";
@@ -26,8 +26,8 @@ const opts = {
 };
 
 // Constants
-const TWITTER_HANDLE = "tom13zebras";
-const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+// const TWITTER_HANDLE = "tom13zebras";
+// const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 
 const AFI_LINK =
@@ -261,17 +261,6 @@ const App = () => {
                     {walletAddress && renderConnectedContainer()}
                 </div>
                 <div className="footer-container">
-                    <img
-                        alt="Twitter Logo"
-                        className="twitter-logo"
-                        src={twitterLogo}
-                    />
-                    <a
-                        className="footer-text"
-                        href={TWITTER_LINK}
-                        target="_blank"
-                        rel="noreferrer"
-                    >{`built by @${TWITTER_HANDLE}`}</a>
                 </div>
             </div>
         </div>
